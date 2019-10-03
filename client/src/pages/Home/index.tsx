4604ccf07fa99f182e15ed8742044aa4b1da523b
Zac Tolley <zac@thetolleys.com>
2019-10-03 16:26:15 +0100

    Put loading message into center of page to avoid jumping around

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
+import { Card, ListGroup, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 import { useTodoListQuery } from '../../graphql'
-import { Card, ListGroup, Container } from 'react-bootstrap'
 
 const Home: React.FC = () => {
   const { data, error, loading } = useTodoListQuery()
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-  if (!data || !data.todos) return <p>No data</p>
-
-  const { todos } = data
+  const todos = !error && !loading && data && data.todos ? data.todos : null
 
   return (
     <Container className="py-3 center" style={{ maxWidth: 300 }}>
       <Card>
-        <ListGroup variant="flush">
-          {todos.map(({ title, id }) => (
-            <ListGroup.Item key={id}>
-              <Link to={`/todo/${id}`}>{title}</Link>
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
+        {loading && <p>Loading...</p>}
+        {error && <p>Error: {error.message}</p>}
+        {todos && (
+          <ListGroup variant="flush">
+            {todos.map(({ title, id }) => (
+              <ListGroup.Item key={id}>
+                <Link to={`/todo/${id}`}>{title}</Link>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
       </Card>
     </Container>
   )
